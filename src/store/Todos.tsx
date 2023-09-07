94b56aa055b37c02d78f1649006959c892a333cc
Jinish Shrestha <dixon.cider014@gmail.com>
2023-09-07 22:23:02 +0545

    Updates

--- src/store/Todos.tsx
+++ src/store/Todos.tsx
@@ -1,4 +1,15 @@
-import { createContext } from "react";
+import { ReactNode, createContext } from 'react'
 
+export type TodosProviderProps = {
+  children: ReactNode
+}
 
+export const todosContext = createContext(null)
 
+export const TodosProvider = ({ children }: TodosProviderProps) => {
+  return (
+    <>
+      <todosContext.Provider value={}>{children}</todosContext.Provider>
+    </>
+  )
+}
